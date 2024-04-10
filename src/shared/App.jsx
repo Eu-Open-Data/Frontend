e661d4840109405dcecf1938bde378d165761f46
Bogdan Mihaica <claudiumihaica12@gmail.com>
2024-04-10 22:18:15 +0300

    test avion

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -15,7 +15,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <div> avion </div>
+        <div> Avion de la bogdan </div>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -30,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
