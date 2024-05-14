c1c0c375fc2604ae2a68898be8e5ae8d9132960c
BalanGabriela05 <gabry.balan05@yahoo.com>
2024-05-14 18:21:20 +0300

    fix: created authentication pages

---
+++ src/pages/Authentication/PagePasswordReset/PasswordReset.jsx
@@ -0,0 +1,24 @@
+
+
+function PasswordReset(){
+
+   
+
+    return(
+            <div className="container-reset">
+                <form action="">
+                    <h1>Password Reset</h1>
+                        <div className="input-box">
+                            <p>Please enter the new password:</p>
+                            <input type="text" placeholder="********" required/>
+                        </div>
+                        <div className="input-box">
+                            <p>Confirm password:</p>
+                            <input type="text" placeholder="********" required/>
+                        </div>
+                    <button type="submit">Reset</button>
+                </form>
+            </div>
+    );
+}
+export default PasswordReset;
