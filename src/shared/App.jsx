a7985c046629496259dc7ecf01637f941db8134e
Bogdan Mihăică <126176262+BogdanMihaica@users.noreply.github.com>
2024-05-15 16:37:43 +0300

    [CU-86bykvaaz] fe02 015 dev  homepage (#30)

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -1,24 +1,10 @@
-
-import "../pages/Authentication/PageSignUp/SignUp.css"
-import SignUp from "../pages/Authentication/PageSignUp/SignUp.jsx";
-import "../pages/Authentication/PageLogIn/LogIn.css"
-import LogIn from "../pages/Authentication/PageLogIn/LogIn.jsx";
-import "../pages/Authentication/PageForgotPassword/ForgotPassword.css"
-import ForgotPassword from "../pages/Authentication/PageForgotPassword/ForgotPassword.jsx";
-import "../pages/Authentication/PagePasswordReset/PasswordReset.css"
-import PasswordReset from "../pages/Authentication/PagePasswordReset/PasswordReset.jsx";
-
+import { LandingPage } from "../pages/Homepage/LandingPage/LandingPage.jsx";
+import "./App.css";
 function App() {
- 
-
   return (
     <div>
-        <SignUp/> <br/>
-        <LogIn/>  <br/>
-        <ForgotPassword/>  <br/>
-        <PasswordReset/>  <br/>
+      <LandingPage />
     </div>
-    
   );
 }
 
