9b948914bdc9ce45b62ba2ab43bd15601d0f188c
BejenaruIoanMatei <128059047+BejenaruIoanMatei@users.noreply.github.com>
2024-05-31 13:40:27 +0300

    [CU-86byxeybb] Adaugare Review la Maps (#46)
    esti barbat

--- src/pages/Map/map-filter-sidebar/SearchOptions.jsx
+++ src/pages/Map/map-filter-sidebar/SearchOptions.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps*/
 import "./SearchOptions.css";
 import getData from "../map-filter-sidebar/fetch.enum.js";
 import fetchData from "../map-filter-sidebar/FiltersController.js";
