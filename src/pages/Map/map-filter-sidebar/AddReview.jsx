f64159ee7cbf992df944e6e4aaef45eb9ae7a8be
Bogdan Mihăică <126176262+BogdanMihaica@users.noreply.github.com>
2024-06-06 16:08:40 +0300

    [CU-86bykvaaz] Brought back indexes  (#53)

--- src/pages/Map/map-filter-sidebar/AddReview.jsx
+++ src/pages/Map/map-filter-sidebar/AddReview.jsx
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
 import "./AddReview.css";
 import SelectableStartIcon from "./SelectableStartIcon.jsx";
 
@@ -8,40 +10,44 @@ export default function AddReview(props) {
   const [hoverRating, setHoverRating] = useState(0);
 
   return (
-      <div className="static" style={{marginTop: '30px'}}>
-        <div className=" bg-white">
+    <div className="static" style={{ marginTop: "30px" }}>
+      <div className=" bg-white">
+        <div className="input-box">
+          <h3>How was the experience?</h3>
 
-          <div className="input-box">
-            <h3>How was the experience?</h3>
+          <input
+            id="add-comment"
+            type="text"
+            placeholder="It was awesome"
+            required
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
 
-            <input
-                id="add-comment"
-                type="text"
-                placeholder="It was awesome"
-                required
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-            />
-          </div>
-
-          <div className="add-rating-container">
-            <div className="add-rating">
-              {Array.from({ length: 5 }, (_, index) => (
-                  <SelectableStartIcon
-                      key={index}
-                      filled={(hoverRating || rating) > index}
-                      onMouseEnter={() => setHoverRating(index + 1)}
-                      onMouseLeave={() => setHoverRating(0)}
-                      onClick={() => setRating(index + 1)}
-                  />
-              ))}
-            </div>
-          </div>
-
-          <div className="write-review">
-            <button className="write-review-button" onClick={() => props.publish(content, rating)}>Publish</button>
+        <div className="add-rating-container">
+          <div className="add-rating">
+            {Array.from({ length: 5 }, (_, index) => (
+              <SelectableStartIcon
+                key={index}
+                filled={(hoverRating || rating) > index}
+                onMouseEnter={() => setHoverRating(index + 1)}
+                onMouseLeave={() => setHoverRating(0)}
+                onClick={() => setRating(index + 1)}
+              />
+            ))}
           </div>
         </div>
+
+        <div className="write-review">
+          <button
+            className="write-review-button"
+            onClick={() => props.publish(content, rating)}
+          >
+            Publish
+          </button>
+        </div>
       </div>
+    </div>
   );
 }
--- src/pages/Map/map-filter-sidebar/SelectableStartIcon.jsx
+++ src/pages/Map/map-filter-sidebar/SelectableStartIcon.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React from "react";
 /* eslint-disable react/prop-types */
-const SelectableStartIcon = ({ filled, onMouseEnter, onMouseLeave, onClick }) => {
-    return (
-        <svg
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}
-            onClick={onClick}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill={filled ? '#FFD700' : '#ccc'}
-            width="24px"
-            height="24px"
-            style={{ cursor: 'pointer' }}
-        >
-            <path d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-        </svg>
-    );
+const SelectableStartIcon = ({
+  filled,
+  onMouseEnter,
+  onMouseLeave,
+  onClick,
+}) => {
+  return (
+    <svg
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onClick={onClick}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill={filled ? "#FFD700" : "#ccc"}
+      width="24px"
+      height="24px"
+      style={{ cursor: "pointer" }}
+    >
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+    </svg>
+  );
 };
 
 export default SelectableStartIcon;
